c0d56cb69999261213b09a99a1d9f30c969f1522
Tony Dias Jr <141673033+programerbro@users.noreply.github.com>
2023-08-28 11:47:14 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,23 +5,15 @@ using namespace std;
 
 int main()
 {
-  cout << " welcome to week four pratice" << endl;
-  //crate a one domensional array
-  char MyChar[10] = {'a','b','c','d','e','f','g','h','i','j'};
-  cout << "\n\n should be an e..." << MyChar[4] << endl;
-  cout << "my char is  " << MyChar << endl;
-  for ( int i =0; i < 10; i++) {
-    cout << "MyChar[" << i << "] is " << MyChar[i] << "\n" << endl;
-  }
-
-  int num1 = 33 ;
-  int num2 = 111 ;
-
-  if (num1 > num2) {
-    cout << " \n\ the test condition was true";
-  }
-  else {
-    cout << "\n the test condition evaluated to false ";
-  }
+    for (int i = 0; i < 5; i++)
+        {
+        cout << "i is " << i << endl;
+    }
+string myStr;
+myStr = "abcdefghij";
+ cout << myStr[0]<< endl;
+ for (int i = 0 ;i < 10; i++){
+    cout << myStr[i] << endl;
+ }
     return 0;
 }
